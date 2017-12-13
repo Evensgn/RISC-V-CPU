1f46fb7b4929add6a644d8e431079fbfe411bc8e
Evensgn <i@evensgn.com>
2017-12-13 22:40:18 +0800

    Update stage_id.v

--- src/defines.v
+++ src/defines.v
@@ -2,12 +2,80 @@
 `define DEFINES_V
 
 //==================  Instruction opcode in RISC-V ================== 
-`define OPCODE_ORI 7'b0010011
+`define OP_IMM 7'b0010011
 
 //================== Instruction funct3 in RISC-V ================== 
 `define FUNCT3_ORI 3'b110
 
-//================== EXE_RES ================== 
+//================== AluOp ==================
+`define EXE_AND_OP  8'b00100100
+`define EXE_OR_OP   8'b00100101
+`define EXE_XOR_OP  8'b00100110
+`define EXE_NOR_OP  8'b00100111
+`define EXE_NOP_OP  8'b00000000
+
+`define EXE_SLL_OP  8'b01111100
+`define EXE_SLLV_OP 8'b00000100
+`define EXE_SRL_OP  8'b00000010
+`define EXE_SRLV_OP 8'b00000110
+`define EXE_SRA_OP  8'b00000011
+`define EXE_SRAV_OP 8'b00000111
+
+`define EXE_MOVZ_OP 8'b00001010
+`define EXE_MOVN_OP 8'b00001011
+`define EXE_MFHI_OP 8'b00010000
+`define EXE_MTHI_OP 8'b00010001
+`define EXE_MFLO_OP 8'b00010010
+`define EXE_MTLO_OP 8'b00010011
+
+`define EXE_SLT_OP   8'b00101010
+`define EXE_SLTU_OP  8'b00101011
+`define EXE_SLTI_OP  8'b01010111
+`define EXE_SLTIU_OP 8'b01011000   
+`define EXE_ADD_OP   8'b00100000
+`define EXE_ADDU_OP  8'b00100001
+`define EXE_SUB_OP   8'b00100010
+`define EXE_SUBU_OP  8'b00100011
+`define EXE_ADDI_OP  8'b01010101
+`define EXE_ADDIU_OP 8'b01010110
+`define EXE_CLZ_OP   8'b10110000
+`define EXE_CLO_OP   8'b10110001
+
+`define EXE_MULT_OP  8'b00011000
+`define EXE_MULTU_OP 8'b00011001
+`define EXE_MUL_OP   8'b10101001
+`define EXE_DIV_OP   8'b00011010
+`define EXE_DIVU_OP  8'b00011011
+
+`define EXE_J_OP      8'b01001111
+`define EXE_JAL_OP    8'b01010000
+`define EXE_JALR_OP   8'b00001001
+`define EXE_JR_OP     8'b00001000
+`define EXE_BEQ_OP    8'b01010001
+`define EXE_BGEZ_OP   8'b01000001
+`define EXE_BGEZAL_OP 8'b01001011
+`define EXE_BGTZ_OP   8'b01010100
+`define EXE_BLEZ_OP   8'b01010011
+`define EXE_BLTZ_OP   8'b01000000
+`define EXE_BLTZAL_OP 8'b01001010
+`define EXE_BNE_OP    8'b01010010
+
+`define EXE_LB_OP  8'b11100000
+`define EXE_LBU_OP 8'b11100100
+`define EXE_LH_OP  8'b11100001
+`define EXE_LHU_OP 8'b11100101
+`define EXE_LL_OP  8'b11110000
+`define EXE_LW_OP  8'b11100011
+`define EXE_LWL_OP 8'b11100010
+`define EXE_LWR_OP 8'b11100110
+`define EXE_SB_OP  8'b11101000
+`define EXE_SC_OP  8'b11111000
+`define EXE_SH_OP  8'b11101001
+`define EXE_SW_OP  8'b11101011
+`define EXE_SWL_OP 8'b11101010
+`define EXE_SWR_OP 8'b11101110
+
+//================== AluSel ================== 
 `define EXE_RES_LOGIC       3'b001
 `define EXE_RES_SHIFT       3'b010
 `define EXE_RES_MOVE        3'b011
@@ -34,5 +102,9 @@
 `define RegBus 31:0
 `define RegNum 32
 `define RegNumLog2 5
+
+// ALU
+`define AluOpBus 
+`define AluSelBus 3
 
 `endif
--- src/stage_id.v
+++ src/stage_id.v
@@ -6,69 +6,62 @@ module stage_id (
 	input  wire [    `InstBus] inst     ,
 	input  wire [     `RegBus] reg_data1,
 	input  wire [     `RegBus] reg_data1,
-	output reg                 reg_re1  ,
-	output reg                 reg_re2  ,
+	output reg                 re1  ,
+	output reg                 re2  ,
 	output reg  [ `RegAddrBus] reg_addr1,
 	output reg  [ `RegAddrBus] reg_addr2,
 	output reg  [   `AluOpBus] aluop    ,
 	output reg  [  `AluSelBus] alusel   ,
-	output reg  [     `RegBus] reg1     ,
-	output reg  [     `RegBus] reg2     ,
-	output reg  [ `RegAddrBus] wd_o     ,
-	output reg                 wreg_o
+	output reg  [     `RegBus] reg1      ,
+	output reg  [     `RegBus] reg2      ,
+	output reg  [ `RegAddrBus] reg_waddr,
+	output reg                 we
 );
 
-	wire[5:0] op = inst_i[31:26];
-	wire[4:0] op2 = inst_i[10:6];
-	wire[5:0] op3 = inst_i[5:0];
-	wire[4:0] op4 = inst_i[20:16];
+	wire[6:0] opcode = inst[6:0];
+	wire[2:0] funct3 = inst[14:12];
+	wire[6:0] funct7 = inst[31:25];
 	reg[`RegBus]	imm;
 	reg instvalid;
 
+	wire[`RegBus] rd = inst[11:7];
+	wire[`RegBus] rs = inst[19:15];
+	wire[`RegBus] rt = inst[24:20];
 
+	`define SET_INST(i_aluop, i_alusel, i_inst_valid, i_re1, i_reg_addr1, i_re2, i_reg_addr2, i_we, i_reg_waddr, i_imm) \
+		aluop <= i_aluop; \
+		alusel <= i_alusel; \
+		inst_valid <= i_inst_valid; \
+		re1 <= i_re1; \
+		reg_addr <= i_reg_addr; \
+		re2 <= i_re2; \
+		reg_addr2 <= i_reg_addr2; \
+		we <= i_we; \
+		reg_waddr <= i_reg_waddr; \
+		imm <= i_imm
+	
 	always @ (*) begin
-		if (rst == `RstEnable) begin
-			aluop_o     <= `EXE_NOP_OP;
-			alusel_o    <= `EXE_RES_NOP;
-			wd_o        <= `NOPRegAddr;
-			wreg_o      <= `WriteDisable;
-			instvalid   <= `InstValid;
-			reg1_read_o <= 1'b0;
-			reg2_read_o <= 1'b0;
-			reg1_addr_o <= `NOPRegAddr;
-			reg2_addr_o <= `NOPRegAddr;
-			imm         <= 32'h0;
+		if (rst) begin
+			`SET_INST(`EXE_NOP_OP, `EXE_RES_NOP, 1, 0, rs, 0, rt, 0, rd, 0);
 		end else begin
-			aluop_o     <= `EXE_NOP_OP;
-			alusel_o    <= `EXE_RES_NOP;
-			wd_o        <= inst_i[15:11];
-			wreg_o      <= `WriteDisable;
-			instvalid   <= `InstInvalid;
-			reg1_read_o <= 1'b0;
-			reg2_read_o <= 1'b0;
-			reg1_addr_o <= inst_i[25:21];
-			reg2_addr_o <= inst_i[20:16];
-			imm         <= `ZeroWord;
-			case (op)
-				`EXE_ORI : begin  
-					wreg_o    <= `WriteEnable;		aluop_o <= `EXE_OR_OP;
-					alusel_o  <= `EXE_RES_LOGIC; reg1_read_o <= 1'b1;	reg2_read_o <= 1'b0;
-					imm       <= {16'h0, inst_i[15:0]};		wd_o <= inst_i[20:16];
-					instvalid <= `InstValid;
+			`SET_INST(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+			case (opcode)
+				`OP_IMM : begin
+					`SET_INST(`EXE_OR_OP, `EXE_RES_LOGIC, 1, 1, rs, 0, 0, 1, rd, ({20'h0, inst[31:20]}));
 				end
 				default : begin
 				end
-			endcase		  //case op
-		end       //if
-	end         //always
+			endcase // op
+		end
+	end
 
 
 	always @ (*) begin
-		if(rst == `RstEnable) begin
+		if(rst) begin
 			reg1_o <= `ZeroWord;
-		end else if(reg1_read_o == 1'b1) begin
+		end else if(re1ad_o == 1'b1) begin
 			reg1_o <= reg1_data_i;
-		end else if(reg1_read_o == 1'b0) begin
+		end else if(re1ad_o == 1'b0) begin
 			reg1_o <= imm;
 		end else begin
 			reg1_o <= `ZeroWord;
@@ -78,9 +71,9 @@ module stage_id (
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			reg2_o <= `ZeroWord;
-		end else if(reg2_read_o == 1'b1) begin
+		end else if(re2ad_o == 1'b1) begin
 			reg2_o <= reg2_data_i;
-		end else if(reg2_read_o == 1'b0) begin
+		end else if(re2ad_o == 1'b0) begin
 			reg2_o <= imm;
 		end else begin
 			reg2_o <= `ZeroWord;
